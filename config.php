0f8507bd6bcb27b53027c28afac5dfd893944624
carlosgarciasc2003@gmail.com <karlos4t>
2024-11-18 19:11:57 +0100

    actualizacion funcion connect db

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
     define('BASE_PATH', __DIR__);
     
-    define('DB_LOCAL_HOST', "localhost");
-    define('DB_LOCAL_USER', "admin");
-    define('DB_LOCAL_PASSWORD', "admin");
-    define('DB_LOCAL_DB', "elarrastre");
+    define('DB_LOCAL_HOST', "elarrastre_mysql_db");
+    define('DB_LOCAL_USER', "carlos");
+    define('DB_LOCAL_PASSWORD', "1234");
+    define('DB_LOCAL_DB', "elarrastre_mysql_db");
 
     define('DB_HOSTINGER_HOST', "localhost");
-    define('DB_HOSTINGER_USER', "u463589001_elarrastre");
-    define('DB_HOSTINGER_PASSWORD', "S9&+!v#4#z");
-    define('DB_HOSTINGER_DB', "u463589001_elarrastre2");
+    define('DB_HOSTINGER_USER', "u237766271_carlos");
+    define('DB_HOSTINGER_PASSWORD', "Ee4/2=Yn");
+    define('DB_HOSTINGER_DB', "u237766271_elarrastre");
     
     //define('DB_HOSTINGER_HOST', "localhost");
     //define('DB_HOSTINGER_USER', "u463589001_user4t");
--- src/back/hostconnect.php
+++ src/back/hostconnect.php
@@ -1,20 +1,32 @@
 <?php
 function hostConnect()
 {
+    // Credenciales de Hostinger
     $host_host = constant("DB_HOSTINGER_HOST");
     $host_user = constant("DB_HOSTINGER_USER");
     $host_key = constant("DB_HOSTINGER_PASSWORD");
     $host_db = constant("DB_HOSTINGER_DB");
     
+    // Credenciales locales
     $local_host = constant("DB_LOCAL_HOST");
     $local_user = constant("DB_LOCAL_USER");
     $local_key = constant("DB_LOCAL_PASSWORD");
     $local_db = constant("DB_LOCAL_DB");
 
-    //$con = mysqli_connect($host_host, $host_user, $host_key, $host_db);
-    $con = mysqli_connect($local_host, $local_user, $local_key, $local_db);
-    if (!$con)
-            die("Error de conexión: " . mysqli_connect_error());
+    // Intentar conexión con las credenciales de Hostinger
+    $con = mysqli_connect($host_host, $host_user, $host_key, $host_db);
+
+    // Si la conexión falla, intentar con las credenciales locales
+    if (!$con) {
+        error_log("Error de conexión a Hostinger: " . mysqli_connect_error());
+        $con = mysqli_connect($local_host, $local_user, $local_key, $local_db);
+
+        // Si también falla la conexión local, detener el script
+        if (!$con) {
+            die("Error de conexión local: " . mysqli_connect_error());
+        }
+    }
+    
     return $con;
 }
 ?>
